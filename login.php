d498e66dc768ea234634f6e825ec9b10a464836b
Siddharth Shankar Choudhary <shankarchoudharysiddharth@gmail.com>
2022-06-13 02:30:12 +0530

    profile

--- login.php
+++ login.php
@@ -143,9 +143,7 @@ require './includes/form_handlers/login_handler.php'; // To include login_handle
 
     <!-- / Content -->
 
-    <div class="buy-now">
-        <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank" class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
-    </div>
+
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
